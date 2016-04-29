80b69edde4da37721c894c739faa32a3a31bfe8a
Prevole <prevole@prevole.ch>
2016-04-29 15:05:08 +0200

    Added the possibility to specify multiple folders to check at the same time

--- index.js
+++ index.js
@@ -7,61 +7,87 @@ var
 ;
 
 if (!process.argv[2]) {
-  console.log('You should specify a root folder.'.red);
+  console.log('You should specify at least one root folder.'.red);
   process.exit(-1);
 }
 
-var rootDirectory = path.resolve(process.argv[2]);
+var rootDirectories = {
+  dirs: process.argv.slice(2),
+  current: 0
+};
+
+_.extend(rootDirectories, {
+  hasNext: function() {
+    return this.current < this.dirs.length;
+  },
+
+  next: function() {
+    return this.dirs[this.current++];
+  }
+});
 
 var filesByName = {};
 var filesByMd5 = {};
 
-fs.walk(rootDirectory)
-  .on('data', function(pathItem) {
-    if (pathItem.stats.isDirectory()) {
-      process.stdout.write('.'.cyan);
-    }
+walk();
 
-    if (pathItem.stats.isFile()) {
-      pathItem.fileName = pathItem.path.replace(/.*\//, '');
-      pathItem.md5 = md5(pathItem.path);
-
-
-      if (_.isUndefined(filesByName[pathItem.fileName])) {
-        filesByName[pathItem.fileName] = [];
+function walk() {
+  fs.walk(rootDirectories.next())
+    .on('data', function(pathItem) {
+      if (pathItem.stats.isDirectory()) {
+        process.stdout.write('.'.cyan);
       }
 
-      filesByName[pathItem.fileName].push(pathItem);
+      if (pathItem.stats.isFile()) {
+        pathItem.fileName = pathItem.path.replace(/.*\//, '');
+        pathItem.md5 = md5(pathItem.path);
 
-      if (_.isUndefined(filesByMd5[pathItem.md5])) {
-        filesByMd5[pathItem.md5] = [];
+
+        if (_.isUndefined(filesByName[pathItem.fileName])) {
+          filesByName[pathItem.fileName] = [];
+        }
+
+        filesByName[pathItem.fileName].push(pathItem);
+
+        if (_.isUndefined(filesByMd5[pathItem.md5])) {
+          filesByMd5[pathItem.md5] = [];
+        }
+
+        filesByMd5[pathItem.md5].push(pathItem);
       }
-
-      filesByMd5[pathItem.md5].push(pathItem);
-    }
-  })
-  .on('end', function() {
-    console.log('Comparison by simple names:'.green);
-    _.each(filesByName, function(fileByName, key) {
-      if (fileByName.length > 1) {
-        console.log(('  It seems the file ' + key + ' is duplicated.').yellow);
-        _.each(fileByName, function(file) {
-          console.log('    ' + file.path.blue + (' (md5: ' + file.md5 + ')').magenta);
-        });
-        console.log('');
+    })
+    .on('end', function() {
+      if (rootDirectories.hasNext()) {
+        walk();
       }
-    });
-
-    console.log('');
-    console.log('Comparison by md5:'.green);
-    _.each(filesByMd5, function(fileByMd5, key) {
-      if (fileByMd5.length > 1) {
-        console.log(('  It seems there is a duplicated file for md5: ' + key).yellow);
-        _.each(fileByMd5, function(file) {
-          console.log('    ' + file.path.blue + (' (md5: ' + file.md5 + ')').magenta);
-        });
-        console.log('');
+      else {
+        print();
       }
     });
-  })
-;
+}
+
+function print() {
+  console.log('\n\n');
+  console.log('Comparison by simple names:'.green);
+  _.each(filesByName, function(fileByName, key) {
+    if (fileByName.length > 1) {
+      console.log(('  It seems the file ' + key + ' is duplicated.').yellow);
+      _.each(fileByName, function(file) {
+        console.log('    ' + file.path.blue + (' (md5: ' + file.md5 + ')').magenta);
+      });
+      console.log('');
+    }
+  });
+
+  console.log('');
+  console.log('Comparison by md5:'.green);
+  _.each(filesByMd5, function(fileByMd5, key) {
+    if (fileByMd5.length > 1) {
+      console.log(('  It seems there is a duplicated file for md5: ' + key).yellow);
+      _.each(fileByMd5, function(file) {
+        console.log('    ' + file.path.blue + (' (md5: ' + file.md5 + ')').magenta);
+      });
+      console.log('');
+    }
+  });
+}
